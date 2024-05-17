66217200c841cbac95a229b51172209f38ef4ce6
salerno.salvatore1997@gmail.com <sallo@MacBook-Pro-di-Salvatore.local>
2024-05-17 08:47:58 +0200

    rewritte generation spawn point

--- scenes/desert/main_desert.gd
+++ scenes/desert/main_desert.gd
@@ -7,20 +7,27 @@ extends Node
 # Every time the timer ticks it instantiate a new lizard
 func _on_timer_timeout():
 	var lizard = lizard_scene.instantiate()
-	lizard.position = get_random_position()
+	lizard.position = sample_point()
 	lizard.initialize()
 	add_child(lizard)
 	#print("nuova lucertola generata")
 
-# Returns a random position within the Path3D
-func get_random_position() -> Vector3:
-	var curve : Curve3D = spawnPath_node.curve
-	var path_length = curve.get_baked_length()
-	var offset = randf_range(0.0, path_length)
-	var result_path_position = curve.sample_baked(offset)
-	var result = spawnPath_node.to_local(result_path_position)
-	result.y = 50.0
-	return result
+# Returns a random point within the generated mesh.
+# A lizard will always spawn from the sky, thus
+# the y-coordinate is a constant
+func sample_point() -> Vector3:
+
+	# Get the size of the desert	
+	var aabb_node = %Desert.get_aabb()
+	var max_x = absf(aabb_node.position.x) - 20  # -20 is done in order
+	var max_z = absf(aabb_node.position.z) - 20  # to prevent that the lizard
+	var max_y = absf(aabb_node.position.y) + 50  # falls off
+
+	var point_x = randi_range(-max_x, max_x) 
+	var point_z = randi_range(-max_z, max_z)
+	
+	# print("generating lizard at x = ", point_x, " z = ", point_z)
+	return Vector3(point_x, max_y, point_z)
 
 func _ready():
 	#print("main_desert è stato caricato yuppi!!")
